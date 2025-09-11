97b25a0c9de224af7865012a0bf390bdba64e84a
cno <carlosnicort@gmail.com>
2025-09-11 12:17:08 +0200

    primer comit - Inicio

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Navbar from "@/components/Navbar"
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <main className="p-8">
+        <h1 className="text-3xl font-bold">Panel de gestión educativa</h1>
+      </main>
+    </>
+  )
+}
